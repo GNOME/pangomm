d6c44208ae2cf5f57614fe099fd51419edf594a1
Kjell Ahlstedt <kjellahlstedt@gmail.com>
2018-12-30 18:44:26 +0100

    tools/gen_scripts: Update for non-source-dir builds
    Most modules (e.g. pango) can be built in a directory separated from the
    source directory. Update the scripts that generate .defs and doc.xml files
    to handle that.
    The environment variable JHBUILD_SOURCES is not used any more.
    Instead the environment variables GMMPROC_GEN_SOURCE_DIR and
    GMMPROC_GEN_BUILD_DIR are read. See comments in init_generate.sh.

--- tools/gen_scripts/generate_all.sh
+++ tools/gen_scripts/generate_all.sh
@@ -2,7 +2,13 @@
 
 # Regenerate all pangomm's docs.xml and .defs files
 
+cd "$(dirname "$0")"
+
+echo === pango_generate_docs.sh ===
 ./pango_generate_docs.sh
+echo === pango_generate_enums.sh ===
 ./pango_generate_enums.sh
+echo === pango_generate_extra_defs.sh ===
 ./pango_generate_extra_defs.sh
+echo === pango_generate_methods.sh ===
 ./pango_generate_methods.sh
--- tools/gen_scripts/pango_generate_extra_defs.sh
+++ tools/gen_scripts/pango_generate_extra_defs.sh
@@ -2,37 +2,10 @@
 
 # The script assumes that it resides in the tools/gen_scripts directory
 # and the defs files will be placed in pango/src.
-# It shall be executed from the tools/gen_scripts directory.
 
-# To update the gtk_signals.defs file:
-# 1. ./pango_generate_extra_defs.sh
-#    Generates pango/src/pango_signals.defs.orig and pango/src/pango_signals.defs.
-#    If any hunks from the patch file fail to apply, apply them manually to the
-#    pango_signals.defs file, if required.
-# 2. Optional: Remove pango/src/pango_signals.defs.orig.
+source "$(dirname "$0")/init_generate.sh"
 
-# To update the pango_signals.defs file and the patch file:
-# 1. Like step 1 when updating only the pango_signals.defs file.
-# 2. Apply new patches manually to the pango_signals.defs file.
-# 3. ./pango_generate_extra_defs.sh --make-patch
-# 4. Like step 2 when updating only the pango_signals.defs file.
+out_dir="$root_dir/pango/src"
 
-ROOT_DIR="$(dirname "$0")/../.."
-GEN_DIR="$ROOT_DIR/tools/extra_defs_gen"
-OUT_DIR="$ROOT_DIR/pango/src"
-OUT_DEFS_FILE="$OUT_DIR"/pango_signals.defs
-
-if [ $# -eq 0 ]
-then
-  # Without LC_ALL=C documentation (docs "xxx") may be translated in the .defs file.
-  LC_ALL=C "$GEN_DIR"/generate_extra_defs > "$OUT_DEFS_FILE"
-  # PATCH_OPTIONS="--backup --version-control=simple --suffix=.orig"
-  # patch $PATCH_OPTIONS "$OUT_DEFS_FILE" "$OUT_DEFS_FILE".patch
-elif [ "$1" = "--make-patch" ]
-then
-  diff --unified=5 "$OUT_DEFS_FILE".orig "$OUT_DEFS_FILE" > "$OUT_DEFS_FILE".patch
-else
-  echo "Usage: $0 [--make-patch]"
-  exit 1
-fi
-
+# Without LC_ALL=C documentation (docs "xxx") may be translated in the .defs file.
+LC_ALL=C "$extra_defs_gen_dir"/generate_extra_defs > "$out_dir"/pango_signals.defs
