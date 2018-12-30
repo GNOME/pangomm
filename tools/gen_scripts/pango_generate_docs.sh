d3b153ad8cb39ead5e03c5c8af18332902ea8c58
Kjell Ahlstedt <kjellahlstedt@gmail.com>
2018-12-30 18:40:10 +0100

    tools/gen_scripts: Update for non-source-dir builds
    Most modules (e.g. pango) can be built in a directory separated from the
    source directory. Update the scripts that generate .defs and doc.xml files
    to handle that.
    The environment variable JHBUILD_SOURCES is not used any more.
    Instead the environment variables GMMPROC_GEN_SOURCE_DIR and
    GMMPROC_GEN_BUILD_DIR are read. See comments in init_generate.sh.

--- tools/gen_scripts/pango_generate_docs.sh
+++ tools/gen_scripts/pango_generate_docs.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-# Note that JHBUILD_SOURCES should be defined to contain the path to the root
-# of the jhbuild sources. The script assumes that it resides in the
-# tools/gen_scripts/ directory and the XML file will be placed in pango/src.
+# The script assumes that it resides in the tools/gen_scripts/ directory and
+# the XML file will be placed in pango/src.
 
-if [ -z "$JHBUILD_SOURCES" ]; then
-  echo -e "JHBUILD_SOURCES must contain the path to the jhbuild sources."
-  exit 1;
-fi
+source "$(dirname "$0")/init_generate.sh"
 
-PREFIX="$JHBUILD_SOURCES"
-ROOT_DIR="$(dirname "$0")/../.."
-OUT_DIR="$ROOT_DIR/pango/src"
+out_dir="$root_dir/pango/src"
 
-PARAMS="--with-properties --no-recursion"
-for dir in "$PREFIX"/pango/pango "$PREFIX"/pango/build/pango; do
+params="--with-properties --no-recursion"
+for dir in "$source_prefix"/pango "$build_prefix"/pango; do
   if [ -d "$dir" ]; then
-    PARAMS="$PARAMS -s $dir"
+    params="$params -s $dir"
   fi
 done
 
-DOCEXTRACT_TO_XML_PY="$JHBUILD_SOURCES/glibmm/tools/defs_gen/docextract_to_xml.py"
-$DOCEXTRACT_TO_XML_PY $PARAMS > "$OUT_DIR/pango_docs.xml"
+"$gen_docs" $params > "$out_dir/pango_docs.xml"
--- tools/gen_scripts/pango_generate_methods.sh
+++ tools/gen_scripts/pango_generate_methods.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-# Note that JHBUILD_SOURCES should be defined to contain the path to the root
-# of the jhbuild sources. The script assumes that it resides in the
-# tools/gen_scripts/ directory and the defs file will be placed in pango/src.
+# The script assumes that it resides in the tools/gen_scripts/ directory
+# and the defs file will be placed in pango/src.
 
-if [ -z "$JHBUILD_SOURCES" ]; then
-  echo -e "JHBUILD_SOURCES must contain the path to the jhbuild sources."
-  exit 1;
-fi
+source "$(dirname "$0")/init_generate.sh"
 
-PREFIX="$JHBUILD_SOURCES/pango"
-ROOT_DIR="$(dirname "$0")/../.."
-OUT_DIR="$ROOT_DIR/pango/src"
+out_dir="$root_dir/pango/src"
 
 shopt -s extglob # Enable extended pattern matching
 shopt -s nullglob # Skip a filename pattern that matches no file
-H2DEF_PY="$JHBUILD_SOURCES/glibmm/tools/defs_gen/h2def.py"
 # Process files whose names end with .h, but not with private.h or internal.h.
-$H2DEF_PY "$PREFIX"/pango/!(*private|*internal).h "$PREFIX"/build/pango/!(*private).h > "$OUT_DIR"/pango_methods.defs
+"$gen_methods" "$source_prefix"/pango/!(*private|*internal).h "$build_prefix"/pango/!(*private).h > "$out_dir"/pango_methods.defs
